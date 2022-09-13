120065b65907bdc1a43eb0ca3331a27e1cec165a
NeGate <yasserarguelles2506@gmail.com>
2022-09-13 18:09:46 -0400

    Update build.lua

--- build.lua
+++ build.lua
@@ -1,6 +1,5 @@
 
--- your based build system stuff goes here
-files = {
+local files = {
     "src/tb/",
     "src/tb/codegen/",
     "src/tb/bigint/",
@@ -12,7 +11,7 @@ files = {
     "src/tb/opt/",
 }
 
-if build.os == "Windows" then
+if config.os == "Windows" then
     table.insert(files, "src/tb/system/win32.c")
 else
     table.insert(files, "src/tb/system/posix.c")
@@ -22,12 +21,13 @@ local options = "-g -Wall -Werror -Wno-unused-function "
 options = options.."-I include "
 options = options.."-I deps/mimalloc/include "
 options = options.."-I deps/luajit/src "
-if build.opt then
+if config.opt then
     options = options.."-O2 -DNDEBUG "
 end
 
-local outputs = build.compile("TB.cache", files, options)
-if outputs ~= nil then
-    print("Step 5: Link")
+local outputs, changes = build.compile("TB.cache", files, options)
+if changes then
     build.lib("tildebackend.lib", "deps/mimalloc-static.lib deps/luajit/src/lua51.lib", outputs)
 end
+
+return changes

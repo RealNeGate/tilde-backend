d63277560e574e1c03da3adc0bc6e37aab957de0
RealNeGate <yasserarguelles@gmail.com>
2022-09-28 14:22:45 -0400

    Switched from GNU atomics to C11 atomics

--- src/tb/tb_atomic.c
+++ src/tb/tb_atomic.c
@@ -34,42 +34,43 @@ void* tb_atomic_ptr_exchange(void** address, void* new_value) {
     return _InterlockedExchangePointer(address, new_value);
 }
 
-void* tb_atomic_ptr_cmpxchg(void** address, void* old_value, void* new_value) {
-    return _InterlockedCompareExchangePointer(address, new_value, old_value);
+bool tb_atomic_ptr_cmpxchg(void** address, void* old_value, void* new_value) {
+    return _InterlockedCompareExchangePointer(address, new_value, old_value) == new_value;
 }
-#elif defined(__GNUC__)
+#elif __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_ATOMICS__)
+#include <stdatomic.h>
 #include <stddef.h>
 
 int tb_atomic_int_load(int* dst) {
-    return __atomic_load_n(dst, __ATOMIC_SEQ_CST);
+    return atomic_load((atomic_int*) dst);
 }
 
 int tb_atomic_int_add(int* dst, int src) {
-    return __atomic_fetch_add(dst, src, __ATOMIC_SEQ_CST);
+    return atomic_fetch_add((atomic_int*) dst, src);
 }
 
 int tb_atomic_int_store(int* dst, int src) {
-    return __atomic_exchange_n(dst, src, __ATOMIC_SEQ_CST);
+    return atomic_exchange((atomic_int*) dst, src);
 }
 
 size_t tb_atomic_size_load(size_t* dst) {
-    return __atomic_load_n(dst, __ATOMIC_SEQ_CST);
+    return atomic_load((atomic_size_t*) dst);
 }
 
 size_t tb_atomic_size_add(size_t* dst, size_t src) {
-    return __atomic_fetch_add(dst, src, __ATOMIC_SEQ_CST);
+    return atomic_fetch_add((atomic_size_t*) dst, src);
 }
 
 size_t tb_atomic_size_store(size_t* dst, size_t src) {
-    return __atomic_exchange_n(dst, src, __ATOMIC_SEQ_CST);
+    return atomic_exchange((atomic_size_t*) dst, src);
 }
 
 void* tb_atomic_ptr_exchange(void** address, void* new_value) {
-    return __atomic_exchange_n(address, new_value, __ATOMIC_SEQ_CST);
+    return atomic_exchange((_Atomic(void*)*) address, new_value);
 }
 
-void* tb_atomic_ptr_cmpxchg(void** address, void* old_value, void* new_value) {
-    return __atomic_compare_exchange(address, old_value, new_value, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+bool tb_atomic_ptr_cmpxchg(void** address, void* old_value, void* new_value) {
+    return atomic_compare_exchange_strong((_Atomic(void*)*) address, &old_value, new_value);
 }
 
 #else

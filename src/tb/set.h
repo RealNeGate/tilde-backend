34ad5ba8e5419e40db908162d6ff2a4f468c33ee
NeGate <yasserarguelles2506@gmail.com>
2022-12-13 20:06:18 -0500

    Moving x64_new to new regalloc

--- src/tb/set.h
+++ src/tb/set.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+#include "builtins.h"
 
 #define SET_INITIAL_CAP 32
 
@@ -26,19 +27,47 @@ inline static void set_clear(Set* s) {
     memset(s->data, 0, ((s->capacity + 63) / 64) * sizeof(uint64_t));
 }
 
-inline static bool set_first_time(Set* s, size_t index) {
-    lldiv_t d = lldiv(index, 64);
-    if (d.quot >= s->capacity) {
-        size_t old = s->capacity;
+inline static size_t set_popcount(Set* s) {
+    size_t sum = 0;
+    for (size_t i = 0; i < s->capacity; i++) {
+        sum += tb_popcount64(s->data[i]);
+    }
 
-        s->capacity = d.quot * 2;
-        s->data = realloc(s->data, s->capacity * sizeof(uint64_t));
+    return sum;
+}
+
+// Grabs a free slot
+inline static ptrdiff_t set_pop_any(Set* s) {
+    size_t slots = (s->capacity + 63) / 64;
+
+    for (size_t i = 0; i < slots; i++) {
+        if (s->data[i] == UINT64_MAX) continue;
+
+        int index = s->data[i] != 0 ? tb_ffs64(~s->data[i]) - 1 : 0;
+        if (i*64 + index >= s->capacity) continue;
+
+        s->data[i] |= (1ull << index);
+        return i*64 + index;
+    }
+
+    return -1;
+}
+
+inline static bool set_first_time(Set* s, size_t index) {
+    size_t slots = (s->capacity + 63) / 64;
+    lldiv_t d = lldiv(index, 64);
+
+    if (d.quot >= slots) {
+        s->capacity = index * 2;
+        size_t new_slots = (s->capacity + 63) / 64;
+
+        s->data = realloc(s->data, new_slots * sizeof(uint64_t));
         if (s->data == NULL) {
             fprintf(stderr, "TB error: Set out of memory!");
             abort();
         }
 
-        memset(s->data + old, 0, (s->capacity - old) * sizeof(uint64_t));
+        memset(s->data + slots, 0, (new_slots - slots) * sizeof(uint64_t));
     }
 
     if ((s->data[d.quot] & (1ull << d.rem)) == 0) {
